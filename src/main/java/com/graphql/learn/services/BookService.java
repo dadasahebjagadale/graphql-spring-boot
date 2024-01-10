8883f73369112db87b9cc047883598f359c05c9e
Dadasaheb Jagadale <dgjagadale@gmail.com>
2024-01-10 20:55:25 +0530

    Initial commit

---
+++ src/main/java/com/graphql/learn/services/BookService.java
@@ -0,0 +1,21 @@
+package com.graphql.learn.services;
+
+import com.graphql.learn.entities.Book;
+
+import java.util.List;
+
+public interface BookService {
+
+    //create
+    Book create(Book book);
+
+    //get all
+    List<Book> getAll();
+
+    //get single book
+    Book get(int bookId);
+    
+    //remove Book
+    Book delete(int bookId);
+
+}
